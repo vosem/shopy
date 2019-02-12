4c1ed5f44fa9e09e2b332bca3059af36be1f7df8
vosem <trefoil2@ukr.net>
2019-02-12 09:53:20 +0200

    added adaptiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-const imageThumbnails = Array.from(document.getElementsByClassName('product-img'));
-const productBigImages = [
-						 'url(\"assets/img/thumbnails_boots_img.png\") center no-repeat',
-						 'url(\"assets/img/thumbnails_trousers_img.png\") center no-repeat',
-						 'url(\"assets/img/big_product_img.png\") center'
-						 ];
-const bigProductImg = document.getElementsByClassName('big-product-img')[0];
+let imageThumbnails = Array.from(document.getElementsByClassName('product-img'));
+let productBigImages = [
+	'url(\"assets/img/thumbnails_boots_img.png\") center no-repeat',
+	'url(\"assets/img/thumbnails_trousers_img.png\") center no-repeat',
+	'url(\"assets/img/big_product_img.png\") center'
+];
+let bigProductImg = document.getElementsByClassName('big-product-img')[0];
 
 for (let img of imageThumbnails) {
 
@@ -22,14 +22,56 @@ for (let img of imageThumbnails) {
 }
 
 const quantity = document.getElementById('quantity-input'),
-	  plusBtn = document.getElementById('quantity-inc-button'),
-	  minusBtn = document.getElementById('quantity-dec-button');
+	plusBtn = document.getElementById('quantity-inc-button'),
+	minusBtn = document.getElementById('quantity-dec-button');
 
 plusBtn.onclick = () => {
-		quantity.value = +quantity.value + 1;
+	quantity.value = +quantity.value + 1;
 };
 minusBtn.onclick = () => {
 	if (quantity.value > 1) {
 		quantity.value = +quantity.value - 1;
 	};
 };
+
+////////////////////// IE //////////////////////////////////////////////////////////////
+// "use strict";
+
+// var imageThumbnails = document.getElementsByClassName('product-img');
+// var imageThumbnailsArray = Array.prototype.slice.call(imageThumbnails, 0);
+// var productBigImages = ['url(\"assets/img/thumbnails_boots_img.png\") center no-repeat', 'url(\"assets/img/thumbnails_trousers_img.png\") center no-repeat', 'url(\"assets/img/big_product_img.png\") center'];
+// var bigProductImg = document.getElementsByClassName('big-product-img')[0];
+
+// var _loop = function _loop() {
+// 	var img = imageThumbnailsArray[_i];
+// 	img.addEventListener('click', function() {
+// 		var imgIndex = imageThumbnailsArray.indexOf(img);
+
+// 		for (var _i2 = 0; _i2 < imageThumbnailsArray.length; _i2++) {
+// 			var _img = imageThumbnailsArray[_i2];
+
+// 			_img.classList.remove('img-active');
+// 		}
+
+// 		img.classList.toggle('img-active');
+// 		bigProductImg.style.background = productBigImages[imgIndex];
+// 	});
+// };
+
+// for (var _i = 0; _i < imageThumbnailsArray.length; _i++) {
+// 	_loop();
+// }
+
+// var quantity = document.getElementById('quantity-input'),
+// 	plusBtn = document.getElementById('quantity-inc-button'),
+// 	minusBtn = document.getElementById('quantity-dec-button');
+
+// plusBtn.onclick = function() {
+// 	quantity.value = +quantity.value + 1;
+// };
+
+// minusBtn.onclick = function() {
+// 	if (quantity.value > 1) {
+// 		quantity.value = +quantity.value - 1;
+// 	}
+// };
